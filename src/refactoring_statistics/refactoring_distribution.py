bae41f1ee6d1bb83fe12a6962f5889de65a7f0f6
Jan Gerling <jan.gerling@posteo.de>
2020-08-14 10:57:49 +0200

    improve log scale axis

--- src/refactoring_statistics/refactoring_distribution.py
+++ src/refactoring_statistics/refactoring_distribution.py
@@ -8,15 +8,37 @@ from os import path
 from pathlib import Path
 
 
-def query_db_commit(table_name: str, refactorings):
+def query_db(table_name: str, refactorings):
     data = []
     for refactoring_name in refactorings:
-        query = f"SELECT `{refactoring_name}` FROM {table_name} WHERE `{refactoring_name}` > 0"
-        refactoringspercommit = execute_query(query).values
-        data.append(refactoringspercommit)
+        if table_name == "refactoringspercommit":
+            query = f"SELECT `{refactoring_name}` FROM {table_name} WHERE `{refactoring_name}` > 0"
+        elif table_name == "refactoringsperclass":
+            query = f"SELECT refactoring_count FROM {table_name} WHERE refactoring LIKE \"{refactoring_name}\""
 
-    log(f"Got the raw data from {table_name} for commits.")
+        refactoring_counts = execute_query(query).values
+        log(f"Got {len(refactoring_counts)} elements from {table_name} for refactoring {refactoring_name}.")
+        data.append(refactoring_counts)
+    log(f"Got the raw data from {table_name} for these refactorings: {refactorings}.")
     return data
+
+
+def plot_distribution(table_name: str, refactorings, descriptor: str, scale: str = "log"):
+    data = query_db(table_name, refactorings)
+
+    fig, ax = plt.subplots(figsize=(max(len(refactorings), 6), 8), dpi=240)
+    ax.set_title(f"Refactoring type distribution with mean per {descriptor} ({str(level)})")
+
+    ax.violinplot(data, widths=0.7, points=1000, showmeans=True, showextrema=True, showmedians=False)
+    plt.xticks(np.arange(len(refactorings)), refactorings, rotation=30)
+    plt.yscale(scale)
+    plt.ylabel(f"Refactoring count")
+    plt.xlabel("Refactoring types")
+    plt.xlabel("Refactoring types")
+    ax.set_ylim(ymin=1)
+    ax.set_yticks(np.arange(1, 11, 1) + [20, 30, 40, 50])
+    fig_path = f"results/Distribution/{descriptor}_{table_name}_{str(level)}_1000_mean_extrema.png"
+    plt.savefig(fig_path)
 
 
 log_init(f"")
@@ -28,17 +50,8 @@ Path(path.dirname("results/Distribution/")).mkdir(parents=True, exist_ok=True)
 for level in Level:
     refactorings = LEVEL_MAP[level]
     if len(refactorings) > 0:
-        #commit
-        data = query_db_commit("refactoringspercommit", refactorings)
-
-        fig, ax = plt.subplots(figsize=(len(refactorings), 8), dpi=240)
-        ax.set_title(f"Refactoring type distribution per commit for (level={str(level)})")
-        ax.violinplot(data, points=1000, showmeans=False, showextrema=False, showmedians=True)
-        plt.xticks(np.arange(len(refactorings)), refactorings, rotation=20)
-        plt.yscale("log")
-        fig_path = f"results/Distribution/commit_refactoringspercommit_{str(level)}.png"
-        plt.savefig(fig_path)
-
+        # plot_distribution("refactoringspercommit", refactorings, "commit")
+        plot_distribution("refactoringsperclass", refactorings, "class", "log")
 
 log(f"Processing statistics took {time.time() - start_time:.2f} seconds.")
 log_close()
